7c8e464bd2ba93b22d2994feabd0bb0860046771
reneix <idvicki@163.com>
2026-04-24 10:30:54 +0800

    Fix HelloWorld output typo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Helo, world!");
-    }
+public static void main(String[] args) {
+System.out.println("Hello, world!");
+}
 }
